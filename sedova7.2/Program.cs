462895c9ebd77445a7141e9826a84faee1382b90
Анна <@DASHA>
2021-11-30 04:48:58 +0200

    Добавьте файлы проекта.

---
+++ sedova7.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sedova7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            string a = Convert.ToString(Console.ReadLine());
+            string res = "";
+            foreach (var item in a)
+            {
+                int n = item;
+                res += item.ToString() + n;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
